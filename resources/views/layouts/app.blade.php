881a20ea0a77d8e748167c367fa69b7ddab0512a
ricardo fuentes <ricarmenta@gmail.com>
2026-05-08 16:56:30 -0700

    feat: self-host Inter font files locally and remove external font service dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,6 @@
 
     <title>{{ config('app.name', 'Formularios') }}</title>
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -7,8 +7,6 @@
 
         <title>{{ $form->title ?? config('app.name', 'Formularios') }}</title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
